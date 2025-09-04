657dc048a848d522e666534d14fe58ca3fe2808c
yk-david <k.yongkeon@gmail.com>
2025-09-04 23:49:24 +0900

    CH2 L3: End

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,21 @@
 import os
 
 def get_files_info(working_directory, directory='.'):
-    full_path = os.path.join(working_directory, directory)
+    target_directory = os.path.join(working_directory, directory)
     abs_working_directory = os.path.abspath(working_directory)
 
-    if not os.path.abspath(full_path).startswith(abs_working_directory):
+    if not os.path.abspath(target_directory).startswith(abs_working_directory):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory' # There still could be `directory` inside other directory than `working_directory`, but it would mean it's not permitted.
-    if not os.path.isdir(full_path):
+    if not os.path.isdir(target_directory):
         return f'Error: "{directory}" is not a directory'
     
     item_description = []
     
-    for item in os.listdir(full_path):
+    for item in os.listdir(target_directory):
         item_description.append(
-            f'- {item}: file_size={os.path.getsize(os.path.join(full_path, item))} bytes, is_dir={os.path.isdir(os.path.join(full_path, item))}'
+            f'- {item}: file_size={os.path.getsize(os.path.join(target_directory, item))} bytes, is_dir={os.path.isdir(os.path.join(target_directory, item))}'
         )
-    
+        
     try:
         if item_description:
             return f'Result for "{directory}" directory:\n{'\n'.join(item_description)}'
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def main():
-    print(get_files_info('calculator', '.'))
-    print(get_files_info('calculator', 'pkg'))
-    print(get_files_info('calculator', '/bin'))
-    print(get_files_info('calculator', '../'))
+def test():
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 
-main()
+if __name__ == '__main__':
+    test()

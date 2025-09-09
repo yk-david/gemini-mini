cdba474ccd1864a99e0edbba0cbfa84ff2ee5bab
yk-david <k.yongkeon@gmail.com>
2025-09-09 16:39:14 +0900

    CH3 L2: End on my own

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from functions.get_files_info import schema_get_files_info
 
 def main():
     if not len(sys.argv) > 1:
@@ -13,22 +14,44 @@ def main():
     api_key = os.environ.get('GEMINI_API_KEY')
 
     client = genai.Client(api_key=api_key)
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     user_prompt = sys.argv[1]
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     messages = [
         types.Content(role='user', parts=[types.Part(text=user_prompt)])
     ]
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info
+        ]
+    )
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages, 
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=system_prompt
+        )
     )
 
     usage_metadata = response.usage_metadata
 
-    print(response.text)
+    # print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f'Calling function: {function_call.name}({function_call.args})')
+
+
 
     if '--verbose' in sys.argv[1:]:
         print(f'User prompt: {user_prompt}')

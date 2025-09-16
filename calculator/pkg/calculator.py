8c9f9c8b09ab984daad207016f321992d577a7ad
yk-david <k.yongkeon@gmail.com>
2025-09-16 13:36:12 +0900

    CH4 L2: End

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -7,7 +7,7 @@ class Calculator:
             "/": lambda a, b: a / b,
         }
         self.precedence = {
-            "+": 1,
+            "+": 3,
             "-": 1,
             "*": 2,
             "/": 2,
